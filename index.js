4f0315fe87c331765284328ffdb20e437c61e0fd
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-01 12:51:52 +0530

    Remove react router and use a toggle button

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 import 'babel-polyfill';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 import SharedElementTransitionGroup from './SharedElementTransitionGroup.js';
+
+class Example extends Component {
+    state = {
+        switch: true,
+    };
+
+    toggle = () => {
+        this.setState((prevState) => ({
+            switch: !prevState.switch,
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.toggle}>Click Me</button>
+                <SharedElementTransitionGroup>
+                    {this.state.switch && <Page1 />}
+                    {!this.state.switch && <Page2 />}
+                </SharedElementTransitionGroup>
+            </div>
+        );
+    }
+}
 
 class Page1 extends Component {
     render() {
@@ -16,9 +39,7 @@ class Page1 extends Component {
         return (
             <div style={this.props.style} ref={this.props.innerRef}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis consequat felis. Donec velit ipsum, laoreet in massa a, volutpat pellentesque augue. Phasellus eu venenatis enim, eget aliquet est. Vestibulum interdum ut sem sed fringilla. Pellentesque aliquam rhoncus mauris et pretium.</p>
-                    <Link to='/element'>
-                        <img id="shared" style={imgStyle} src='https://68.media.tumblr.com/4d1f173744a32bb4b35a2d5d0babff74/tumblr_mnh29fxz111st5lhmo1_1280.jpg' />
-                    </Link>
+                <img id="hero" style={imgStyle} src='https://68.media.tumblr.com/4d1f173744a32bb4b35a2d5d0babff74/tumblr_mnh29fxz111st5lhmo1_1280.jpg' />
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis consequat felis. Donec velit ipsum, laoreet in massa a, volutpat pellentesque augue.</p>
             </div>
         );
@@ -34,7 +55,7 @@ class Page2 extends React.Component {
 
         return (
             <div style={this.props.style} ref={this.props.innerRef}>
-                <img id="shared" style={imgStyle} src='https://68.media.tumblr.com/4d1f173744a32bb4b35a2d5d0babff74/tumblr_mnh29fxz111st5lhmo1_1280.jpg' />
+                <img id="hero" style={imgStyle} src='https://68.media.tumblr.com/4d1f173744a32bb4b35a2d5d0babff74/tumblr_mnh29fxz111st5lhmo1_1280.jpg' />
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis consequat felis. Donec velit ipsum, laoreet in massa a, volutpat pellentesque augue. Phasellus eu venenatis enim, eget aliquet est. Vestibulum interdum ut sem sed fringilla. Pellentesque aliquam rhoncus mauris et pretium. Pellentesque vehicula malesuada risus, ut laoreet lectus dignissim vel. Suspendisse vel ex id tortor tincidunt rhoncus. In tempor commodo sapien placerat auctor. Maecenas sollicitudin augue massa. Quisque facilisis dui quis risus tempor euismod pharetra ut leo. Fusce sit amet dui at nisi facilisis lobortis ut ut est. Morbi a arcu eget nibh aliquam maximus sed quis ex.</p>
             </div>
         );
@@ -42,15 +63,6 @@ class Page2 extends React.Component {
 }
 
 ReactDOM.render(
-    <BrowserRouter basename="/react-layout-transition">
-        <Route path='/' render={({match, location, ...rest}) => {
-            return (
-                <SharedElementTransitionGroup>
-                    {location.pathname === '/' && match.isExact && <Page1 />}
-                    {location.pathname === '/element' && <Page2 />}
-                </SharedElementTransitionGroup>
-            );
-        }} />
-    </BrowserRouter>,
-document.getElementById('root')
+    <Example />,
+    document.getElementById('root')
 );
