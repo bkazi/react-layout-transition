004d02ebdb3ebcda65c02c114d8d38ecd75ed6e2
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-06 16:01:32 +0530

    Add example for layout transition group

--- example/index.js
+++ example/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 
-import Example from './src/Example';
+// import Example from './src/Example';
+import AnotherOne from './src/AnotherOne';
 
 const render = (Component) => {
   ReactDOM.render(
@@ -14,11 +15,15 @@ const render = (Component) => {
   );
 };
 
-render(Example);
+render(AnotherOne);
 
 if (module.hot) {
-  module.hot.accept('./src/Example', () => {
-    const NewApp = require('./src/Example').default;
+  // module.hot.accept('./src/Example', () => {
+  //   const NewApp = require('./src/Example').default;
+  //   render(NewApp);
+  // });
+  module.hot.accept('./src/AnotherOne', () => {
+    const NewApp = require('./src/AnotherOne').default;
     render(NewApp);
   });
 }
---
+++ example/src/AnotherOne.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {LayoutTransitionGroup} from '../../index.js';
+
+class AnotherOne extends LayoutTransitionGroup {
+    state = {
+        count: 3,
+    };
+
+    increment = () => {
+        this.beginTransition(this.listRef, (prevState) => ({
+            count: prevState.count + 1,
+        }));
+    };
+
+    render() {
+        const linearStyle = {
+            minHeight: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+        };
+
+        const childStyle = (i) => ({
+            textAlign: 'center',
+            width: '100%',
+            padding: '16px',
+            backgroundColor: `rgb(${(60*(i+1)) % 255}, 50, ${(40 * (i+1)) % 255})`,
+        });
+
+        return (
+            <div>
+                <button onClick={this.increment}>Click Me</button>
+                <div 
+                    style={linearStyle}
+                    ref={(ref) => {
+                        this.listRef = ref;
+                    }}
+                >
+                    {[...Array(this.state.count).keys()].map((i) => <div style={childStyle(i)} key={i}>{i}</div>)}
+                </div>
+            </div>
+        );
+    }
+}
+
+// const LayoutWrapper = (props) => {
+//     return (
+        
+//     );
+// };
+
+export default AnotherOne;
