90c0eacfd1c89619bc22eda48877e97260e82703
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-08 23:34:44 +0530

    Add footer component

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <span>Made by <a href='https://www.twitter.com/bkazi'>@bkazi</a></span>
+        </div>
+    );
+};
+
+export default Footer;
