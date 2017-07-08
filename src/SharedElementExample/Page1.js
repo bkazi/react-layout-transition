32c3c076797a9c058a52afde13337ef736b436a9
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-08 14:39:37 +0530

    Remove inline styles

--- src/SharedElementExample/Page1.js
+++ src/SharedElementExample/Page1.js
@@ -2,31 +2,12 @@ import React from 'react';
 
 class Page1 extends React.Component {
     render() {
-        const cont = {
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-            overflow: 'hidden',
-        };
-
-        const img1Style = {
-            maxWidth: '100%',
-            height: 'auto',
-        };
-
-        const img2Style = {
-            maxWidth: '100%',
-            height: 'auto',
-        };
-
         return (
             <div style={this.props.style} ref={this.props.innerRef}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis consequat felis. Donec velit ipsum, laoreet in massa a, volutpat pellentesque augue. Phasellus eu venenatis enim, eget aliquet est. Vestibulum interdum ut sem sed fringilla. Pellentesque aliquam rhoncus mauris et pretium.</p>
-                <div style={cont}>
-                    <img id="hero" style={img1Style} src='https://68.media.tumblr.com/4d1f173744a32bb4b35a2d5d0babff74/tumblr_mnh29fxz111st5lhmo1_1280.jpg' />
-                    <img id="another-one" style={img2Style} src='http://wefunction.com/wordpress/wp-content/uploads/2013/10/tumblr_ms9mpnB2o61st5lhmo1_1280.jpg' />
+                <div className='horizontalFlex'>
+                    <img id="hero" className='example-image' src='https://68.media.tumblr.com/4d1f173744a32bb4b35a2d5d0babff74/tumblr_mnh29fxz111st5lhmo1_1280.jpg' />
+                    <img id="another-one" className='example-image' src='http://wefunction.com/wordpress/wp-content/uploads/2013/10/tumblr_ms9mpnB2o61st5lhmo1_1280.jpg' />
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis consequat felis. Donec velit ipsum, laoreet in massa a, volutpat pellentesque augue.</p>
             </div>
