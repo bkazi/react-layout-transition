96a9e7b99722076fe81af2d64c55cd950dc58eb4
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-21 18:55:32 +0530

    Add minified and non-minified umd build

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,16 @@ const path = require('path');
 const webpack = require('webpack');
 const BabiliPlugin = require('babili-webpack-plugin');
 
+const isProd = process.env.NODE_ENV === 'PRODUCTION';
+
+const outputFilename = isProd ? 'react-layout-transition.min.js' : 'react-layout-transition.js';
+
 module.exports = {
+    devtool: !isProd ? 'source-map' : '',
     entry: path.resolve(__dirname, 'src/index.ts'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'react-layout-transition.js',
+        filename: outputFilename,
         library: 'ReactLayoutTransition',
         libraryTarget: 'umd',
     },
@@ -29,12 +34,12 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
-    plugins: [
+    plugins: isProd ? [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         new BabiliPlugin({}, {
             removeComments: true,
         }),
-    ],
+    ] : [],
 };
