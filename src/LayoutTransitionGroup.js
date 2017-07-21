59cde6eed11344352d71195c94397f1c2be7bc9b
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-21 10:20:18 +0530

    Add option for custom timing and easing curve

--- src/LayoutTransitionGroup.js
+++ src/LayoutTransitionGroup.js
@@ -3,14 +3,16 @@ import React from 'react';
 import fireOnce from './utils/fireOnce.js';
 
 class LayoutTransitionGroup extends React.Component {
-    initialDimens = new Map();
-    finalDimens = new Map();
+    _lInitialDimens = new Map();
+    _lfinalDimens = new Map();
     state = {
-        _transitionPending: false,
-        _transitionRefs: undefined,
+        _lTransitionPending: false,
+        _lTransitionRefs: undefined,
+        _lTransitionTiming: undefined,
+        _lTransitionEasing: undefined,
     }
 
-    beginTransition = (stateUpdateFn, refs) => {
+    beginTransition = (stateUpdateFn, refs, timing = 200, easing = 'ease-in-out') => {
         if (refs.constructor !== Array) refs = [refs];
         // Traverse top layers for inital positions
         const childNodes = [].concat(...refs.map((ref) => Array.from(ref.childNodes)));
@@ -19,26 +21,28 @@ class LayoutTransitionGroup extends React.Component {
             const key = `.${index}`;
             child.setAttribute('data-layout-key', key);
 
-            this.initialDimens.set(key, child.getBoundingClientRect());
+            this._lInitialDimens.set(key, child.getBoundingClientRect());
         });
 
         // Update state
         this.setState((prevState) => ({
             ...stateUpdateFn(prevState),
-            _transitionPending: true,
-            _transitionRefs: refs,
+            _lTransitionPending: true,
+            _lTransitionRefs: refs,
+            _lTransitionTiming: timing,
+            _lTransitionEasing: easing,
         }));
     };
 
     componentDidUpdate(prevProps, prevState) {
-        if (!this.state._transitionPending) return;
+        if (!this.state._lTransitionPending) return;
 
         // Traverse top layer for final positions
-        const refs = this.state._transitionRefs;
+        const refs = this.state._lTransitionRefs;
         const childNodes = [].concat(...refs.map((ref) => Array.from(ref.childNodes)));
         childNodes.forEach((child) => {
             if (!child.dataset.layoutKey) return;
-            this.finalDimens.set(child.dataset.layoutKey, child.getBoundingClientRect());
+            this._lfinalDimens.set(child.dataset.layoutKey, child.getBoundingClientRect());
         });
 
         // Fix existing nodes in same place
@@ -49,8 +53,8 @@ class LayoutTransitionGroup extends React.Component {
                 child.style.pointerEvents = 'none';
                 return;
             };
-            const initialDimen = this.initialDimens.get(key);
-            const finalDimen = this.finalDimens.get(key);
+            const initialDimen = this._lInitialDimens.get(key);
+            const finalDimen = this._lfinalDimens.get(key);
             const x = initialDimen.left - finalDimen.left;
             const y = initialDimen.top - finalDimen.top;
             const sx = initialDimen.width / finalDimen.width;
@@ -62,6 +66,8 @@ class LayoutTransitionGroup extends React.Component {
 
         // Transition
             // Animate into new position
+        const timing = this.state._lTransitionTiming;
+        const easing = this.state._lTransitionEasing;
         requestAnimationFrame(() => {
             requestAnimationFrame(() => {
                 const initialNodes = [];
@@ -71,7 +77,7 @@ class LayoutTransitionGroup extends React.Component {
                         newNodes.push(child);
                         return;
                     }
-                    child.style.transition = 'transform 200ms ease-in-out';
+                    child.style.transition = `transform ${timing}ms ${easing}`;
                     child.style.transform = '';
                     initialNodes.push(child);
                 });
@@ -90,9 +96,10 @@ class LayoutTransitionGroup extends React.Component {
         });
 
         this.setState((state) => ({
-            _transitionPending: false,
-            _transitionRefs: undefined,
-            _transitionInitialDimens: undefined,
+            _lTransitionPending: false,
+            _lTransitionRefs: undefined,
+            _lTransitionTiming: undefined,
+            _lTransitionEasing: undefined,
         }));
     }
 
