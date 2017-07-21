01e5e1425d8200ef8d53cdf821799702d6885ceb
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-21 15:02:15 +0530

    Make use of types

--- src/utils/fireOnce.ts
+++ src/utils/fireOnce.ts
@@ -1,8 +1,8 @@
-export default function fireOnce(nodes, eventType, func) {
+export default function fireOnce(nodes: Array<HTMLElement>, eventType: string, func: Function) {
     const totalCount = nodes.length;
     let count = 0;
-    const events = [];
-    const eventListener = (event) => {
+    const events: Array<Event> = [];
+    const eventListener = (event: Event) => {
         count++;
         events.push(event);
         if (count === totalCount) {
