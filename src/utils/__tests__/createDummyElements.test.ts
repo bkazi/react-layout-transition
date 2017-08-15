fca5efd05dd16b3d4adeba01a8a30526a353ecae
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-08-15 21:03:56 +0530

    Split into different tests
    Although writing these tests has lead me to take what I said back, maybe Jest isn't the best option.

--- src/utils/__tests__/createDummyElements.test.ts
+++ src/utils/__tests__/createDummyElements.test.ts
@@ -1,16 +1,58 @@
 import {createDummyElements} from '../';
 
-test('should create one dummy element', () => {
-    const elements = [document.createElement('div')];
-    const bcrs = elements.map(el => el.getBoundingClientRect());
+describe('createDummyElements (single)', () => {
+    const elementTag = 'DIV';
+    const width = 0;
+    const height = 0;
 
-    const returned = createDummyElements(elements, bcrs);
+    function mockSingleElement() {
+        const element = document.createElement(elementTag);
 
-    const returnedEl = returned[0];
-    const style = getComputedStyle(returnedEl);
+        const elements = [element];
 
-    expect(returnedEl.tagName).toEqual('DIV');
-    expect(style.position).toEqual('absolute');
-    expect(style.width).toEqual('0px');
-    expect(style.height).toEqual('0px');
+        return elements;
+    }
+
+    test('should have same tag name', () => {
+        const elements = mockSingleElement();
+
+        const returned = createDummyElements(
+            elements,
+            elements.map(el => el.getBoundingClientRect()),
+        );
+
+        const returnedEl = returned[0];
+        const style = getComputedStyle(returnedEl);
+
+        expect(returnedEl.tagName).toEqual(elementTag);
+    });
+
+    test('should have position set to absoulte', () => {
+        const elements = mockSingleElement();
+
+        const returned = createDummyElements(
+            elements,
+            elements.map(el => el.getBoundingClientRect()),
+        );
+
+        const returnedEl = returned[0];
+        const style = getComputedStyle(returnedEl);
+
+        expect(style.position).toEqual('absolute');
+    });
+
+    test('should have width and height set properly', () => {
+        const elements = mockSingleElement();
+
+        const returned = createDummyElements(
+            elements,
+            elements.map(el => el.getBoundingClientRect()),
+        );
+
+        const returnedEl = returned[0];
+        const style = getComputedStyle(returnedEl);
+
+        expect(style.width).toEqual(`${width}px`);
+        expect(style.height).toEqual(`${height}px`);
+    });
 });
