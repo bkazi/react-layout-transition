8fbe8ce7eb77ce1572da8e0992148de83dee9975
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-08-21 13:40:51 +0530

    Fix array equality
    probably most embarrasing bug of my life so far

--- src/utils/children.ts
+++ src/utils/children.ts
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import compareArray from './compareArray';
+
 function childrenToMap(children?: any): Map<string, React.ReactElement<any>> {
     const childMap: Map<string, React.ReactElement<any>> = new Map();
     if (!children) {
@@ -17,10 +19,13 @@ function compareChildren(
     children1: Map<string, React.ReactNode>,
     children2: Map<string, React.ReactNode>,
 ): boolean {
+    if (children1.size === 0 || children2.size === 0) {
+        return false;
+    }
     const keys1 = Array.from(children1.keys());
     const keys2 = Array.from(children2.keys());
 
-    return keys1 === keys2;
+    return compareArray(keys1, keys2);
 }
 
 export {childrenToMap, compareChildren};
