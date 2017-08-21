8e4a324834d6fbd3cf2fadeb761660b35a884eee
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-08-21 18:00:43 +0530

    Add test to check if it works without animations

---
+++ tests/SharedElementTransitionGroup.test.tsx
@@ -0,0 +1,56 @@
+import {mount} from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+import * as React from 'react';
+
+import {SharedElementTransitionGroup} from '../src';
+
+describe('SharedElementTransitionGroup', () => {
+    const Foo = (props) => (
+        <div>Foo</div>
+    );
+    const Bar = (props) => (
+        <div>Bar</div>
+    );
+    class MyComponent extends React.Component {
+        public state: {
+            toggle: boolean,
+        };
+        constructor() {
+            super();
+
+            this.state = {
+                toggle: true,
+            };
+        }
+
+        public render() {
+            return (
+                <SharedElementTransitionGroup>
+                    {this.state.toggle && <Foo />}
+                    {!this.state.toggle && <Bar />}
+                </SharedElementTransitionGroup>
+            );
+        }
+    }
+
+    beforeEach(() => {
+        jasmineEnzyme();
+    });
+
+    it('should render the correct elements', () => {
+        const wrapper = mount(<MyComponent />);
+
+        expect(wrapper.find('Foo')).toBePresent();
+        expect(wrapper.find('Bar')).not.toBePresent();
+    });
+
+    it('should render the correct elements', () => {
+        const wrapper = mount(<MyComponent />);
+        wrapper.setState({
+            toggle: false,
+        });
+
+        expect(wrapper.find('Foo')).not.toBePresent();
+        expect(wrapper.find('Bar')).toBePresent();
+    });
+});
