417e905517d64220db79afa3e5295ad9375cbe25
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-08-21 12:25:44 +0530

    Add another test for no children

--- tests/utils/children.test.tsx
+++ tests/utils/children.test.tsx
@@ -25,4 +25,11 @@ describe('childrenToMap', () => {
             expect(child0Wrapper.text()).toEqual(child0Text);
         }
     });
+
+    it('should return an empty map if no children are passed', () => {
+        const childMap = childrenToMap(null);
+
+        expect(childMap).not.toBeNull();
+        expect(childMap.size).toEqual(0);
+    });
 });
