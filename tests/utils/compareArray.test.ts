4b1aeb91746b0f845a6ced5a9d9a8fbf40543c74
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-08-21 13:40:11 +0530

    Add function to check equality of 2 arrays

---
+++ tests/utils/compareArray.test.ts
@@ -0,0 +1,17 @@
+import compareArray from '../../src/utils/compareArray';
+
+describe('compareArray', () => {
+    it('should return false if arrays are different', () => {
+        const x = [1, 3, 5, 7];
+        const y = [2, 4, 6, 8];
+
+        expect(compareArray(x, y)).toEqual(false);
+    });
+
+    it('should return true if arrays have same values', () => {
+        const x = [1, 2, 3, 4];
+        const y = [1, 2, 3, 4];
+
+        expect(compareArray(x, y)).toEqual(true);
+    });
+});
